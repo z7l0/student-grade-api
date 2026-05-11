93c70f594f7aee25b54916248d6f99908dfafba5
Muteb <Muteb@gmail.com>
2026-05-11 20:25:41 +0300

    Break pipeline: change health to DOWN

--- src/main/java/com/example/grades/StudentController.java
+++ src/main/java/com/example/grades/StudentController.java
@@ -42,6 +42,6 @@ public class StudentController {
     @GetMapping("/health")
     public Map<String, String> health() {
         logger.info("Health check called");
-        return Map.of("status", "UP");
+        return Map.of("status",  "DOWN");
     }
 }
